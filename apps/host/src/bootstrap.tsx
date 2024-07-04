e73135812e23129ef8251b8a80ae994f42e07be3
Zaur Ismailov <zaur.ismailov.dev@gmail.com>
2024-07-04 10:06:57 -0400

    Add publishable react lib

--- apps/host/src/bootstrap.tsx
+++ apps/host/src/bootstrap.tsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { browserRouter } from './app/router';
-
+import { MyThemeProvider } from '@elrick85/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  
+  <MyThemeProvider>
     <RouterProvider router={browserRouter} />
-  
+  </MyThemeProvider>
 );
