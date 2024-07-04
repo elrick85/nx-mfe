5f7f95f5b9842b9d131bb353ab9646c58b4f5e44
Zaur Ismailov <zaur.ismailov.dev@gmail.com>
2024-07-04 10:57:54 -0400

    chore(release): publish 0.0.2

---
+++ libs/theme-1/README.md
@@ -0,0 +1,7 @@
+# theme-1
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test theme-1` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/theme-1/src/lib/theme-1.tsx
@@ -0,0 +1,11 @@
+import styles from './theme-1.module.scss';
+
+export function Theme1() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Theme1!</h1>
+    </div>
+  );
+}
+
+export default Theme1;
